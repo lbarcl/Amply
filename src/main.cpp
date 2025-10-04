c57444c2de95d3da8a6d250fcc77cc70b9f65e68
Barçın <ersoybarcin@gmail.com>
2025-10-05 01:44:46 +0300

    Add GainEffect to DigitalAmp initialization
    A GainEffect instance is now created and added to the DigitalAmp's effects list. The CommandHandler is also given a reference to the GainEffect, enabling command-based manipulation.

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,14 @@
 #include "digitalamp.h"
 
 int main() { 
+    std::shared_ptr<GainEffect> gain = std::make_shared<GainEffect>(2.0f);
+
     DigitalAmp amp;
     amp.initialize();
+    amp.effects.push_back(gain);
 
     CommandHandler cmd(&amp);
+    cmd.gainEffect = gain;
     cmd.run();
 
     return 0;
